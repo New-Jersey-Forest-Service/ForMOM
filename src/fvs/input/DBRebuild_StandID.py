f0059eab325c8011f6e13e5c5b4a4a977747503d
MrGorbunov <volarcko@gmail.com>
2022-04-25 11:25:46 -0400

    Added all current progress automating the db
    - DBRebuild_Main: Table deleting, updating groupaddfiles, calling other scripts
    - DBRebuild_StandID: Used to extract Stand IDs from the three tables
    - commandblock1 & 2: SQL commands to build part of the db
    
    There are also some working files which will be removed
    from the final repo. There's a jupyter notebook as well as
    two .txt files with manual directions for building the db

---
+++ src/fvs/input/DBRebuild_StandID.py
@@ -0,0 +1,84 @@
+'''
+This script generates the Replace_ID tables that are needed to swap out
+the FIA IDs with stand IDs for FVS.
+
+It will actually modify the database, so there is no need for 
+extra csvs, etc.
+
+Note, this script does not create duplicate tables 'IDReplace'
+
+Michael Gorbunov
+"FVSStandID.ipynb" by Bill Zipse was used as reference
+'''
+
+import sqlite3
+
+# TODO: Validate 
+#   - [ ] Check table names for table_name
+#   - [ ] Check that for_type_ind does indeed have "ForType="
+#   - [ ] Check that stand_cn uniquely maps to a forest type
+#   - [ ] Check that all stand_cns are covered (esp with TREEINIT)
+# TODO: Less magic numbers
+#   - [ ] Somehow explain or derive the 7
+# TODO: Less magic names
+#	- [ ] Use dataclases or attrs classes instead of dicts
+# TODO: Better integration
+#	- [ ] Rewrite the structs to use cur.executemany()
+
+def extract_forest_types(cur: sqlite3.Cursor, tables: list):
+	# creates a map from STAND_CN (int) -> for_type (str)
+	stand_fortype_map = {}
+
+	for table_name in tables:
+		query = f'SELECT GROUPS, STAND_CN FROM {table_name}'
+		cur.execute(query)
+
+		for row in cur:
+			stand_cn = int(row[1])
+			str_groups = str(row[0]).split(" ")
+			FOR_TYPE_IND = 7
+			# This is making a lot of assumptions about the db :(
+			for_type = str(str_groups[FOR_TYPE_IND]).split("=")[1]
+
+			if not stand_cn in stand_fortype_map.keys():
+				stand_fortype_map[stand_cn] = for_type
+	
+	return stand_fortype_map
+
+
+def do_replacement(cur: sqlite3.Cursor, table_name:str, cn_to_fortype_dict: list) -> None:
+	for ind, key in enumerate(list(cn_to_fortype_dict.keys())):
+		stand_cn = key
+		for_type = cn_to_fortype_dict[key]
+
+		query = f'''
+			UPDATE {table_name} SET STAND_ID = "{for_type}"
+			WHERE STAND_CN = {stand_cn}
+		'''
+		cur.execute(query)
+
+		if (ind % 1000 == 0):
+			print(f'Executed 100 queries')
+
+
+def do_id_replace(cur: sqlite3.Cursor):
+	# Actual processing
+	fortype_dict = extract_forest_types(cur, ["FVS_STANDINIT_PLOT", "FVS_PLOTINIT_PLOT"])
+
+	# Now do replacements
+	do_replacement(cur, "FVS_PLOTINIT_PLOT", fortype_dict)
+	do_replacement(cur, "FVS_STANDINIT_PLOT", fortype_dict)
+	do_replacement(cur, "FVS_TREEINIT_PLOT", fortype_dict)
+
+
+if __name__ == '__main__':
+	DB_PATH = "./FIADB_NJ.db"
+	db_connection = sqlite3.connect(DB_PATH)
+	cur = db_connection.cursor()
+
+	do_id_replace(cur)
+
+	db_connection.commit()
+	db_connection.close()
+
+
---
+++ src/fvs/input/commandblock2.sql
@@ -0,0 +1,192 @@
+---
+--- Creating and Populate the FVS_STANDINIT_PLOT_FT table
+---
+CREATE TABLE FVS_STANDINIT_PLOT_FT (
+    STAND_ID VARCHAR (26),
+    VARIANT VARCHAR (11),
+    INV_YEAR FLOAT,
+    --- The [...] are to delimit the inside incase you wanted spaces or smthg
+    --- it's not really necessary
+    --- see: https://stackoverflow.com/questions/9917196/meaning-of-square-brackets-in-ms-sql-table-designer
+    [GROUPS] VARCHAR (200),
+    ADDFILES VARCHAR (200),
+    FVSKEYWORDS VARCHAR (200),
+    REGION FLOAT,
+    FOREST FLOAT,
+    LOCATION FLOAT,
+    AGE FLOAT,
+    BASAL_AREA_FACTOR FLOAT,
+    INV_PLOT_SIZE FLOAT,
+    BRK_DBH FLOAT,
+    NUM_PLOTS FLOAT,
+    SAM_WT FLOAT,
+    DG_TRANS FLOAT,
+    DG_MEASURE FLOAT,
+    SITE_SPECIES VARCHAR (8),
+    SITE_INDEX FLOAT,
+    STATE FLOAT --- No comma at the end !!!
+);
+
+--- Populate ID
+INSERT INTO
+    FVS_STANDINIT_PLOT_FT (STAND_ID)
+SELECT
+    DISTINCT STAND_ID
+FROM
+    FVS_PLOTINIT_PLOT;
+
+--- Remove '/' character ("n/a" => "na")
+UPDATE
+    FVS_STANDINIT_PLOT_FT
+SET
+    STAND_ID = REPLACE(STAND_ID, '/', '') -- Could just be "na"
+WHERE
+    STAND_ID = "n/a";
+
+
+
+-- AVG_AGE has 58 rows ???  => 105, 800 are added ???
+-- This is because it's cominf from STANDINIT while the IDs came from PLOTINIT
+
+--- Set Average Ages
+CREATE VIEW IF NOT EXISTS AVG_AGE AS
+SELECT
+    STAND_ID,
+    ROUND(AVG(AGE)) AS AVERAGE_AGE
+FROM
+    FVS_STANDINIT_PLOT
+GROUP BY
+    STAND_ID;
+
+UPDATE
+    FVS_STANDINIT_PLOT_FT
+SET
+    AGE = (
+        SELECT
+            AVERAGE_AGE
+        FROM
+            AVG_AGE
+        WHERE
+            AVG_AGE.STAND_ID = FVS_STANDINIT_PLOT_FT.STAND_ID
+    );
+
+
+--- Site Species
+-- Step 1: Get info from STANDINIT
+CREATE VIEW STANDINIT_CORES_BY_FT AS
+SELECT
+    STAND_ID,
+    SITE_SPECIES,
+    SITE_INDEX,
+    COUNT(SITE_SPECIES) AS NUM_CORINGS,
+    AVG(SITE_INDEX) AS AVG_SI
+FROM
+    FVS_STANDINIT_PLOT
+WHERE
+    SITE_SPECIES > 1
+GROUP BY
+    STAND_ID,
+    SITE_SPECIES
+ORDER BY
+    STAND_ID,
+    -NUM_CORINGS;
+
+
+-- This only has 48 distinct stand_ids ??
+CREATE VIEW STANDINIT_MAX_CORES_PER_FT_WITH_TIES AS
+SELECT
+    ALL_FT.*
+FROM
+    STANDINIT_CORES_BY_FT AS ALL_FT
+    INNER JOIN (
+        SELECT
+            STAND_ID,
+            MAX(NUM_CORINGS) AS MAX_CORINGS
+        FROM
+            STANDINIT_CORES_BY_FT
+        GROUP BY
+            STAND_ID
+    ) AS MAX_PER_FT ON ALL_FT.STAND_ID = MAX_PER_FT.STAND_ID
+    AND ALL_FT.NUM_CORINGS = MAX_PER_FT.MAX_CORINGS;
+
+
+-- Tree View (used for tie breaks)
+
+-- Count number of species per stand
+CREATE VIEW TREEINIT_SPECIES_BY_FT AS
+SELECT TREEINIT.STAND_ID, 
+	TREEINIT.SPECIES AS SITE_SPECIES, 
+	COUNT(TREEINIT.SPECIES) AS NUM_SPECIES 
+FROM FVS_TREEINIT_PLOT AS TREEINIT
+GROUP BY TREEINIT.STAND_ID, 
+	TREEINIT.SPECIES;
+
+-- Generate table of target num_species ( only has 47 rows though :(, 46 if distinct stand_id )
+CREATE VIEW TREEINIT_MAX_SPECIES_PER_FT AS
+SELECT STAND_ID, MAX(NUM_SPECIES) AS MAX_NUM_SPECIES
+FROM TREEINIT_SPECIES_BY_FT INNER JOIN
+	STANDINIT_MAX_CORES_PER_FT_WITH_TIES
+USING (STAND_ID, SITE_SPECIES)
+GROUP BY STAND_ID;
+
+
+-- Huh so we do end up with 49 rows in the end, I wonder which table that's coming from 
+-- Something is wrong right now, there are certain stands getting null SITE_SPECIES values :(
+
+-- These two really can be turned into a natural join but izzz ok for now
+
+CREATE VIEW STANDINIT_MAX_WITH_NUM_SPECIES AS
+SELECT STAND_MAX.STAND_ID, STAND_MAX.SITE_SPECIES, TREE_SPECIES.NUM_SPECIES
+FROM STANDINIT_MAX_CORES_PER_FT_WITH_TIES AS STAND_MAX
+	INNER JOIN TREEINIT_SPECIES_BY_FT AS TREE_SPECIES
+ON STAND_MAX.STAND_ID = TREE_SPECIES.STAND_ID AND
+	STAND_MAX.SITE_SPECIES = TREE_SPECIES.SITE_SPECIES;
+
+
+CREATE VIEW STANDINIT_STAND_ID_SITE_SPECIES AS 
+SELECT S.STAND_ID, S.SITE_SPECIES, S.NUM_SPECIES
+FROM STANDINIT_MAX_WITH_NUM_SPECIES AS S
+	INNER JOIN TREEINIT_MAX_SPECIES_PER_FT AS T
+ON
+	S.STAND_ID = T.STAND_ID 
+	AND S.NUM_SPECIES = T.MAX_NUM_SPECIES;
+
+UPDATE FVS_STANDINIT_PLOT_FT
+SET SITE_SPECIES = 
+   (SELECT SITE_SPECIES 
+	FROM STANDINIT_STAND_ID_SITE_SPECIES
+	WHERE FVS_STANDINIT_PLOT_FT.STAND_ID = STANDINIT_STAND_ID_SITE_SPECIES.STAND_ID);
+
+-- TODO: Figure out where the 9 rows are being filtered down from, 
+--	for now though I just cut out with
+DELETE FROM FVS_STANDINIT_PLOT_FT
+WHERE SITE_SPECIES IS NULL;
+
+-- I have NULL values for Stand_IDs
+-- STAND_ID	AGE		SITE_SPECIES
+-- "381"	"41.0"		NULL
+-- "406"	"90.0"		NULL
+-- "501"	"-54.0"		NULL
+-- "509"	"45.0"		NULL
+-- "514"	"2.0"		NULL
+-- "703"	"5.0"		NULL
+-- "902"	"19.0"		NULL
+-- "995"	"29.0"		NULL
+-- "na"		NULL		NULL
+--
+-- (9 rows)
+
+
+
+
+-- 
+-- 
+
+
+
+
+
+
+
+
+
