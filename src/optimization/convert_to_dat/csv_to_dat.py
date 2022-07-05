6d74c7b945d518ea173bc86a50491ef66f33037a
MrGorbunov <volarcko@gmail.com>
2022-07-04 21:46:17 -0400

    [BUG] CSV to Dat now exports correct objective coeffs

--- src/optimization/convert_to_dat/csv_to_dat.py
+++ src/optimization/convert_to_dat/csv_to_dat.py
@@ -111,7 +111,10 @@ def convertInputToFinalModel (objData: models.InputObjectiveData, constData: mod
 
 	# Actually populating the lists
 	var_names = constData.var_names
-	obj_coeffs = objData.obj_coeffs
+	obj_coeffs = [0.0] * len(objData.obj_coeffs)
+	for ind, name in enumerate(objData.var_names):
+		coef_ind = var_names.index(name)
+		obj_coeffs[coef_ind] = objData.obj_coeffs[ind]
 
 	for ind, name in enumerate(constData.const_names):
 		# TODO: This .lower().strip() should happen in the linting step
