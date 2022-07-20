cc4d56f85075f871ddf9365022b9ec07c973edff
MrGorbunov <volarcko@gmail.com>
2022-07-20 00:36:10 -0400

    Monospace font (for outputs) + lil logo

--- src/optimization/model_runner/gui.py
+++ src/optimization/model_runner/gui.py
@@ -39,7 +39,7 @@ class GuibuildingApp:
 		self.state = GUIState()
 
 		# build ui
-		self.im_a_top = master if master != None else tk.Tk()
+		self.im_a_top = master if master is not None else tk.Tk()
 		self.frm_title = ttk.Frame(self.im_a_top)
 		self.lbl_title = ttk.Label(self.frm_title)
 		self.lbl_title.configure(text="ForMOM Linear Model Runner")
@@ -111,7 +111,9 @@ class GuibuildingApp:
 		self.frm_actualrunning.columnconfigure(0, minsize=300)
 		self.lblfrm_status = ttk.Labelframe(self.im_a_top)
 		self.txt_status = tk.Text(self.lblfrm_status)
-		self.txt_status.configure(width=50, wrap="word")
+		self.txt_status.configure(undo="true", width=50)
+		_text_ = '	 \n\n\n			888\'Y88	\n			888 ,\'Y  e88 88e  888,8,\n			888C8   d888 888b 888 " \n			888 "   Y888 888P 888   \n			888	  "88 88"  888   \n \n		 e   e	   e88 88e	   e   e\n		d8b d8b	 d888 888b	 d8b d8b\n	   e Y8b Y8b   C8888 8888D   e Y8b Y8b\n	  d8b Y8b Y8b   Y888 888P   d8b Y8b Y8b\n	 d888b Y8b Y8b   "88 88"   d888b Y8b Y8b\n\n  \n'
+		self.txt_status.insert("0.0", _text_)
 		self.txt_status.grid(column=0, padx=10, pady=10, row=0, sticky="nsew")
 		self.lblfrm_status.configure(height=200, text="Status", width=200)
 		self.lblfrm_status.grid(column=1, padx=20, pady=10, row=1, sticky="nsew")
@@ -271,6 +273,9 @@ class GuibuildingApp:
 		
 		# Text sizes
 		self.lbl_title.configure(font=("Arial", 18))
+
+		# Make the status box monospace
+		self.txt_status.configure(font='TkFixedFont')
 
 
 
